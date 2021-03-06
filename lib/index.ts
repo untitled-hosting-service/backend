10f3500ce40a3efcc5e31c5fbe74d8c2464fc9dc
Jerred Shepherd <ShepherdJerred@gmail.com>
2021-03-05 21:50:17 -0800

    Fix linter

--- lib/index.ts
+++ lib/index.ts
@@ -1,12 +1,19 @@
-import {v4 as uuidv4} from "uuid";
-import {ApolloServer as ApolloLambdaServer} from "apollo-server-lambda";
-import {ApolloServer} from "apollo-server";
+import { v4 as uuidv4 } from "uuid";
+import { ApolloServer as ApolloLambdaServer } from "apollo-server-lambda";
+import { ApolloServer } from "apollo-server";
 import words from "random-words";
-import {Server, ServerStatus, User, typeDefs} from "@mira-hq/model/dist/index";
+import {
+  Server,
+  ServerStatus,
+  User,
+  typeDefs,
+} from "@mira-hq/model/dist/index";
 
-const servers: Server[] = Array(100).fill(undefined).map(() => {
-  return generateRandomServer();
-})
+const servers: Server[] = Array(100)
+  .fill(undefined)
+  .map(() => {
+    return generateRandomServer();
+  });
 
 function generateRandomServer(): Server {
   return {
@@ -17,15 +24,15 @@ function generateRandomServer(): Server {
     maxUptime: getRandomInt(0, 10000),
     uptime: getRandomInt(0, 10000),
     address: `${words()}.mira-hq.com`,
-    owner: generateRandomUser()
-  }
+    owner: generateRandomUser(),
+  };
 }
 
 function generateRandomUser(): User {
   return {
     uuid: uuidv4(),
-    email: `${words()}@gmail.com`
-  }
+    email: `${words()}@gmail.com`,
+  };
 }
 
 // Generating random whole numbers in JavaScript in a specific range?
@@ -39,10 +46,10 @@ function getRandomInt(min: number, max: number) {
 // How to get a random enum in TypeScript?
 // https://stackoverflow.com/questions/44230998/how-to-get-a-random-enum-in-typescript
 function randomEnum<T>(anEnum: T): T[keyof T] {
-  const enumValues = Object.keys(anEnum)
-    .map(n => Number.parseInt(n))
-    .filter(n => !Number.isNaN(n)) as unknown as T[keyof T][]
-  const randomIndex = Math.floor(Math.random() * enumValues.length)
+  const enumValues = (Object.keys(anEnum)
+    .map((n) => Number.parseInt(n))
+    .filter((n) => !Number.isNaN(n)) as unknown) as T[keyof T][];
+  const randomIndex = Math.floor(Math.random() * enumValues.length);
   return enumValues[randomIndex];
 }
 
@@ -54,15 +61,17 @@ const resolvers = {
 
 const isProduction = false;
 
-if (isProduction) {
-  const server = new ApolloLambdaServer({typeDefs: typeDefs, resolvers});
+const server: ApolloLambdaServer = new ApolloLambdaServer({
+  typeDefs: typeDefs,
+  resolvers,
+});
 
-  exports.graphqlHandler = server.createHandler();
-} else {
-  const server = new ApolloServer({typeDefs: typeDefs, resolvers});
+export const graphQlHandler = server.createHandler();
 
-  server.listen().then(({url}) => {
+if (!isProduction) {
+  const server = new ApolloServer({ typeDefs: typeDefs, resolvers });
+
+  void server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
   });
 }
-
